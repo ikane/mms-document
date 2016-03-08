00637a94d7a658efbf232f29c4b7dec909e7afa5
ikane <kaneelhadji2610@yahoo.fr>
2016-03-08 10:50:39 +0100

    Update app.js

--- src/main/webapp/resources/app/app.js
+++ src/main/webapp/resources/app/app.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var mmsDocument = angular.module("mmsDocument", ["ngAnimate", "ngTouch", "ui.bootstrap", "ngRoute"]);
+
+var mmsDocument = angular.module("mmsDocument", ["ngAnimate", "ngTouch", "ui.bootstrap", "ngRoute", "ngFileUpload"]);
+
 
 mmsDocument.config(function($routeProvider) {
 	$routeProvider
 		.when('/', {controller:'MainController', templateUrl:'resources/app/partials/mms-document-search.html'})
 		.when('/mms-document-detail', {controller:'MmsDocumentDetailsController', templateUrl:'resources/app/partials/mms-document-details.html'})
+		.when('/info', {controller:'MainController', templateUrl:'resources/app/partials/mms-document-info.html'})
 		.otherwise({redirectTo:'/'})
 		;
 });
@@ -27,6 +30,34 @@ function MainController($scope, $http) {
 		;
 }
 
-function MmsDocumentDetailsController($scope, $http) {
+
+function MmsDocumentDetailsController($scope, $http, Upload) {
 	
+	$scope.mmsDocument={};
+	
+	$scope.saveDocument = function() {
+		//mmsDocumentFile
+		
+		//alert('saving...');
+		console.log("saving document...");
+		
+		if ($scope.mmsDocumentForm.mmsDocumentFile.$valid && $scope.mmsDocumentFile) {
+	        $scope.upload($scope.mmsDocumentFile);
+	    }
+	};
+	
+	// upload on file select or drop 
+    $scope.upload = function (file) {
+        Upload.upload({
+            url: 'uploadMmsDocument',
+            data: {file: file, 'documentTitle': $scope.mmsDocument.title, 'documentDescription': $scope.mmsDocument.description}
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        });
+    };	
 }
